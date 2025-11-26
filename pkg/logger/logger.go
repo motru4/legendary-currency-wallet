e13b30b5a1445a652cdc607bf4756592047bd567
motru4 <motru4dmitre4@gmail.com>
2025-11-26 16:45:05 +0300

    build(pkg/logger): add structured logging package

---
+++ pkg/logger/logger.go
@@ -0,0 +1,28 @@
+package logger
+
+import (
+	"log/slog"
+	"os"
+)
+
+func New(level string) *slog.Logger {
+	var slogLevel slog.Level
+	switch level {
+	case "debug":
+		slogLevel = slog.LevelDebug
+	case "info":
+		slogLevel = slog.LevelInfo
+	case "warn":
+		slogLevel = slog.LevelWarn
+	case "error":
+		slogLevel = slog.LevelError
+	default:
+		slogLevel = slog.LevelInfo
+	}
+
+	handler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slogLevel,
+	})
+
+	return slog.New(handler)
+}
